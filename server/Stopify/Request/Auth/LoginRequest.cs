e41f4e45222af544df0b678c181e8fb9722308f4
Gabeloeba <samechten4@gmail.com>
2025-01-18 00:30:48 +0100

    Be able to use email or username to log in

--- server/Stopify/Request/Auth/LoginRequest.cs
+++ server/Stopify/Request/Auth/LoginRequest.cs
@@ -1,7 +1,10 @@
 ﻿namespace Stopify.Request.Auth;
 
-public class LoginRequest(string username, string password)
+public class LoginRequest(string? username, string? email, string password)
 {
-    public string Username { get; set; } = username;
+    public string? Username { get; set; } = username;
+    
+    public string? Email { get; set; } = email;
     public string Password { get; set; } = password;
+
 }
