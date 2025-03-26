dad421a6ca379b2193ef22da68ed1d534d1a6f87
Bram Jansen <bramjansen99@gmail.com>
2025-03-26 22:02:48 +0100

    Init blazor project

--- client/clientStopify/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace clientStopify;
-
-public partial class MainPage : ContentPage
-{
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-}
--- client/clientStopify/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace clientStopify;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
