8e70e7df3172703276dd384e4a08c374a1c4f947
Sam Echten <56364108+SamEchten@users.noreply.github.com>
2026-03-18 16:42:10 +0100

    DEV: Client (#18)
    * init blazer maui
    
    * Init blazor project
    
    * Start command
    
    * Make file
    
    ---------
    
    Co-authored-by: Bram Jansen <bramjansen99@gmail.com>

---
+++ client/Stopify/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Stopify
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Stopify" };
+        }
+    }
+}
